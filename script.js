0710c10d8461d6e278114efdbc5bc31c822e2a9d
Dinesh M <68711555+dinesh-raina@users.noreply.github.com>
2021-01-26 17:09:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// swiper
+var swiper = new Swiper('.swiper-container', {
+    effect: 'flip',
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+//-------------------------------------------------------------------------
+
